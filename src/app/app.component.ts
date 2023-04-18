d75696c23e41fdf08e613e2340825cb732eec041
dharani-199 <dharani.k@thinkbridge.in>
2023-04-18 15:27:21 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,64 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent implements OnInit{
+//   title = 'reg-page';
+//   event_form:any
+//   todayISOString: string = new Date().toISOString();
+//   ngOnInit(){
+//     this.event_form = new FormGroup({
+//       First_name: new FormControl('', [Validators.required]),
+//       Last_name: new FormControl('', [Validators.required]),
+//       BusinessEmail: new FormControl('', [Validators.required]),
+//       CompanyName: new FormControl('', [Validators.required]),
+//       Companysize: new FormControl('', [Validators.required]),
+//       message: new FormControl('', [Validators.required]),
+//       check: new FormControl('', [Validators.required]),
+//       selectoptionValue: new FormControl('',[Validators.required])
+  
+//     });
+//   }
+
+ 
+// }
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reg-page';
-
-  todayISOString: string = new Date().toISOString();
+export class AppComponent implements OnInit {
+  todayISOString : string = new Date().toISOString();
   event_form = new FormGroup({
-    First_name: new FormControl('', [Validators.required]),
-    Last_name: new FormControl('', [Validators.required]),
-    BusinessEmail: new FormControl('', [Validators.required]),
-    CompanyName: new FormControl('', [Validators.required]),
-    Companysize: new FormControl('', [Validators.required]),
-    message: new FormControl('', [Validators.required]),
-    check: new FormControl('', [Validators.required]),
+    First_name : new FormControl('',[Validators.required]),
+    Last_name : new FormControl('',[Validators.required]),
+    BusinessEmail : new FormControl('',[Validators.required]),
+    CompanyName : new FormControl('',[Validators.required]),
+    message: new FormControl('',[Validators.required]),
+    check: new FormControl('',[Validators.required])
 
-  });
+
+    })
+
+  languages: any = "Angular";
+  @Input() inputData: any
+  @Output() datas = new EventEmitter()
+
+  ngOnInit() {
+
+  }
+
+  onclick() {
+    this.datas.emit(this.languages)
+  }
+
+
 }
+
